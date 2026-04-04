84508c4dbe2ccdce228277fdd175ee5b4a01f608
Vladimir Herdman <vovaherdman@gmail.com>
2026-04-03 21:53:02 -0400

    MainMenu class callable through MainApi. General Setup.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
+"""Python implementation of the 32bit-based Visual Basic timekeeping
+application. Changes made to work on 64bit Windows 11 systems.
+
+Table of Contents:
+    MainApi  - Class bridge between the frontend and 'backend' for calling
+               Python code.
+    MainMenu - Class that modularizes MainApi. Contains menu functions.
+    main()   - Function at bottom of file that creates webview window and
+               starts program.
+
+All other functions and classes used, while important, I've deemed
+easily understandable if seen, either from their naming or use. Take
+the above as a table of contents for the 'important' sections that may
+need to be easily found.
+"""
 import webview
 from webview.menu import Menu, MenuAction
 
 
 class MainApi:
-    """Called by javascript in frontend to execute python."""
+    """Main bridge for frontend javascript to execute python."""
     def __init__(self):
         self._window: webview.Window|None = None
+        self.menu: MainMenu = MainMenu(self)
 
     @property
     def window(self) -> webview.Window:
@@ -15,13 +31,47 @@ class MainApi:
     def window(self, win):
         self._window = win
 
-    def menu_file_main(self):
-        print("TODO: 'menu_file_main()' functionality")
-    def menu_file_exit(self):
-        self.window.destroy()
-
     def nullfunc(self, file:str) -> None:
         print(f"nullfunc called from file:{file.split("/")[-1]}")
+
+class MainMenu:
+    def __init__(self, api: MainApi):
+        self.api = api
+
+    def file_main(self):
+        print("TODO: 'file_main()' functionality")
+    def file_exit(self):
+        self.api.window.destroy()
+
+    def timekeeping_addRun(self):
+        print("TODO: 'timekeeping_addRun()' functionality")
+    def timekeeping_findDeleteRun(self):
+        print("TODO: 'timekeeping_findDeleteRun()' functionality")
+
+    def employeeRecord_addEmployee(self):
+        print("TODO: 'employeeRecord_addEmployee()' functionality")
+    def employeeRecord_findEditEmployee(self):
+        print("TODO: 'employeeRecord_findEditEmployee()' functionality")
+
+    def positions_salaried_addPosition(self):
+        print("TODO: 'positions_salaried_addPosition()' functionality")
+    def positions_salaried_findEditPayRate(self):
+        print("TODO: 'positions_salaried_findEditPayRate()' functionality")
+    def positions_payRates_addPayRate(self):
+        print("TODO: 'positions_payRates_addPayRate()' functionality")
+    def positions_payRates_findEditPayRate(self):
+        print("TODO: 'positions_payRates_findEditPayRate()' functionality")
+
+    def reports_quarterlyReports(self):
+        print("TODO: 'reports_quarterlyReports()' functionality")
+    def reports_employeeMasterList(self):
+        print("TODO: 'reports_employeeMasterList()' functionality")
+    def reports_salariedJobSummary(self):
+        print("TODO: 'reports_salariedJobSummary()' functionality")
+    def reports_payRateSummary(self):
+        print("TODO: 'reports_payRateSummary()' functionality")
+    def reports_runSummary(self):
+        print("TODO: 'reports_runSummary()' functionality")
 
 
 def build_menu(api: MainApi):
@@ -29,45 +79,45 @@ def build_menu(api: MainApi):
         Menu(
             "File",
             [
-                MenuAction("Main", api.menu_file_main),
-                MenuAction("Exit", api.menu_file_exit),
+                MenuAction("Main", api.menu.file_main),
+                MenuAction("Exit", api.menu.file_exit),
             ],
         ),
         Menu(
             "Timekeeping",
             [
-                MenuAction("Add Run", lambda: api.nullfunc(__file__)),
-                MenuAction("Find/Delete Run", lambda: api.nullfunc(__file__)),
+                MenuAction("Add Run", api.menu.timekeeping_addRun),
+                MenuAction("Find/Delete Run", api.menu.timekeeping_findDeleteRun),
             ],
         ),
         Menu(
             "Employee Record",
             [
-                MenuAction("Add Employee", lambda: api.nullfunc(__file__)),
-                MenuAction("Find/Edit Employee", lambda: api.nullfunc(__file__)),
+                MenuAction("Add Employee", api.menu.employeeRecord_addEmployee),
+                MenuAction("Find/Edit Employee", api.menu.employeeRecord_findEditEmployee),
             ],
         ),
         Menu(
             "Positions",
             [
                 Menu("Salaried", [
-                    MenuAction("Add Position", lambda: api.nullfunc(__file__)),
-                    MenuAction("Find/Edit Pay Rate", lambda: api.nullfunc(__file__)),
+                    MenuAction("Add Position", api.menu.positions_salaried_addPosition),
+                    MenuAction("Find/Edit Pay Rate", api.menu.positions_salaried_findEditPayRate),
                 ]),
                 Menu("Pay Rates", [
-                    MenuAction("Add Pay Rate", lambda: api.nullfunc(__file__)),
-                    MenuAction("Find/Edit Pay Rate", lambda: api.nullfunc(__file__)),
+                    MenuAction("Add Pay Rate", api.menu.positions_payRates_addPayRate),
+                    MenuAction("Find/Edit Pay Rate", api.menu.positions_payRates_findEditPayRate),
                 ]),
             ],
         ),
         Menu(
             "Reports",
             [
-                MenuAction("Quarterly Reports", lambda: api.nullfunc(__file__)),
-                MenuAction("Employee Master List", lambda: api.nullfunc(__file__)),
-                MenuAction("Salaried Job Summary", lambda: api.nullfunc(__file__)),
-                MenuAction("Pay Rate Summary", lambda: api.nullfunc(__file__)),
-                MenuAction("Run Summary", lambda: api.nullfunc(__file__)),
+                MenuAction("Quarterly Reports", api.menu.reports_quarterlyReports),
+                MenuAction("Employee Master List", api.menu.reports_employeeMasterList),
+                MenuAction("Salaried Job Summary", api.menu.reports_salariedJobSummary),
+                MenuAction("Pay Rate Summary", api.menu.reports_payRateSummary),
+                MenuAction("Run Summary", api.menu.reports_runSummary),
             ],
         ),
     ]
@@ -76,7 +126,7 @@ def build_menu(api: MainApi):
 def main():
     api = MainApi()
 
-    win = webview.create_window(
+    api.window = webview.create_window(
         title="Ada EMS Timekeeping",
         url="./ui/index.html",
         js_api=api,
@@ -84,9 +134,7 @@ def main():
         menu=build_menu(api),
     )
 
-    api.window = win
     webview.start()
-
 
 if __name__ == "__main__":
     main()
